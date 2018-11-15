57f13052115fb999cd1924e794e74a3b52a7c2d4
liuvasconcelos <liu.arquiteta@gmail.com>
2018-11-15 17:30:44 -0300

    feature/#1 Add search bar to look for movies.
    - Also added a error or empty treatment.
    
    Signed-off-by: liuvasconcelos <liu.arquiteta@gmail.com>

--- favorite-movies/Application/Modules/MoviesSearch/MoviesSearchViewController.swift
+++ favorite-movies/Application/Modules/MoviesSearch/MoviesSearchViewController.swift
@@ -12,6 +12,9 @@ class MoviesSearchViewController: UIViewController, MoviesSearchViewContract {
     public static let NIB_NAME = "MoviesSearchViewController"
     
     @IBOutlet weak var moviesSearchTableView: MoviesSearchTableView!
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var buttonLabel: UIButton!
+    @IBOutlet weak var errorOrEmptyMessage: UILabel!
     
     lazy var presenter: MoviesSearchPresenterContract = {
         return MoviesSearchPresenter(view: self,
@@ -24,8 +27,14 @@ class MoviesSearchViewController: UIViewController, MoviesSearchViewContract {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        setErrorOrEmpty(message: "Por favor, realize uma busca.")
         
-        presenter.searchMoviesBy("sogra")
+        buttonLabel.setTitle("Procurar", for: .normal)
+    }
+    
+    @IBAction func searchMovies(_ sender: Any) {
+        let search = searchTextField.text ?? ""
+        presenter.searchMoviesBy(search)
     }
     
     func show(movies: [MovieResponse]) {
@@ -33,16 +42,23 @@ class MoviesSearchViewController: UIViewController, MoviesSearchViewContract {
             self.showEmptyMessage()
             return
         }
-        
+        moviesSearchTableView.isHidden = false
+        errorOrEmptyMessage.isHidden = true
         moviesSearchTableView.set(movies: movies)
         moviesSearchTableView.reloadData()
     }
     
     fileprivate func showEmptyMessage() {
-        print("vazio")
+        setErrorOrEmpty(message: "Nenhum resultado")
     }
     
     func show(error: String) {
-        print("erro")
+        setErrorOrEmpty(message: "Ocorreu um erro. Tente novamente.")
+    }
+    
+    fileprivate func setErrorOrEmpty(message: String) {
+        moviesSearchTableView.isHidden = true
+        errorOrEmptyMessage.isHidden   = false
+        errorOrEmptyMessage.text       = message
     }
 }
