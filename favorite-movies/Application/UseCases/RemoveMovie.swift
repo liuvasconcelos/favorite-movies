adc80623644c6f026b63e029432800b063b4ede1
liuvasconcelos <liu.arquiteta@gmail.com>
2018-11-17 22:09:47 -0300

    feature/#1 Add realm structure to save and show favorite movies.
    -
    
    Signed-off-by: liuvasconcelos <liu.arquiteta@gmail.com>

---
+++ favorite-movies/Application/UseCases/RemoveMovie.swift
@@ -0,0 +1,9 @@
+//
+//  RemoveMovie.swift
+//  favorite-movies
+//
+//  Created by Livia Vasconcelos on 17/11/18.
+//  Copyright © 2018 Livia Vasconcelos. All rights reserved.
+//
+
+import Foundation
