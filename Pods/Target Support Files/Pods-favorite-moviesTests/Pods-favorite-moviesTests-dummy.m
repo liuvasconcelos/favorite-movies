6f3767a86425a6ba21cb9717d95e9afecc06ab2e
Livia Vasconcelos <ltenoriovasconcelos@gmail.com>
2018-11-21 09:33:58 -0300

    develop Add git ignore to project
    -
    
    Signed-off-by: Livia Vasconcelos <ltenoriovasconcelos@gmail.com>

--- Pods/Target Support Files/Pods-favorite-moviesTests/Pods-favorite-moviesTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_favorite_moviesTests : NSObject
-@end
-@implementation PodsDummy_Pods_favorite_moviesTests
-@end
--- Pods/Target Support Files/Pods-favorite-moviesUITests/Pods-favorite-moviesUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_favorite_moviesUITests : NSObject
-@end
-@implementation PodsDummy_Pods_favorite_moviesUITests
-@end
